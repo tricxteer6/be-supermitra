cfe3803785c87bf40fde11e0bb8da8607f9487d0
Baihaqi <baihaqirazzak26@gmail.com>
2026-01-21 17:42:06 +0700

    fixing db

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,13 @@
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: "127.0.0.1", // ⬅️ WAJIB
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 module.exports = db;
