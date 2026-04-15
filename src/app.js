1c6aba653cb1ba384761c868843ecf32cc74eec2
Baihaqi <baihaqirazzak26@gmail.com>
2026-04-15 09:03:12 +0700

    fixing bugs

--- src/app.js
+++ src/app.js
@@ -106,5 +106,5 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+  console.log("Server running on porta", PORT);
 });
